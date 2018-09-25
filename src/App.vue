8e074d7cc09d1bcf145e48c162ffdeac7bbce745
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-25 13:04:16 +0100

    step 5

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <v-app>
-    <v-toolbar>
+    <v-navigation-drawer v-model="sideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>directions_run</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>View Funruns</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark absolute class="primary">
+      <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
       <v-toolbar-title>
-        <v-icon>whatshot</v-icon>
-        Funrun</v-toolbar-title>
+        <v-icon>whatshot</v-icon>funrun</v-toolbar-title>
+
       <v-spacer></v-spacer>
-      <v-toolbar-items>
+      <v-toolbar-items class="hidden-xs-only">
         <v-btn flat>
           <v-icon left>directions_run</v-icon>
           View Funruns</v-btn>
@@ -21,7 +32,7 @@
 export default {
   data () {
     return {
-
+      sideNav: false
     }
   }
 }
