4ccd3c9de49864a10ae7a83ba3b952d96cccda82
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-25 13:47:45 +0100

    working nav links and routes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="sideNav">
+    <v-navigation-drawer temporary v-model="sideNav">
       <v-list>
-        <v-list-tile v-for="item in menuItems" :key="item.title">
+        <v-list-tile
+        v-for="item in menuItems"
+        :key="item.title"
+        router
+        :to="item.link"
+        >
+
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
@@ -13,11 +19,18 @@
     <v-toolbar dark absolute class="primary">
       <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
       <v-toolbar-title>
-        <v-icon>whatshot</v-icon>funrun</v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer"><v-icon>whatshot</v-icon>
+        funrun</router-link>
+      </v-toolbar-title>
 
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat v-for="item in menuItems" :key="item.title">
+        <v-btn flat
+        v-for="item in menuItems"
+        :key="item.title"
+        router
+        :to="item.link"
+        >
           <v-icon left dark>{{ item.icon }}</v-icon>
           {{ item.title }}</v-btn>
       </v-toolbar-items>
@@ -34,11 +47,11 @@ export default {
     return {
       sideNav: false,
       menuItems: [
-        { icon: 'directions_run', title: 'View Funruns' },
-        { icon: 'room', title: 'Organise Funrun' },
-        { icon: 'person', title: 'Profile' },
-        { icon: 'face', title: 'Sign up' },
-        { icon: 'lock_open', title: 'Sign in' },
+        { icon: 'directions_run', title: 'View Funruns', link: '/funruns' },
+        { icon: 'room', title: 'Organise Funrun', link: '/meetup/new' },
+        { icon: 'person', title: 'Profile', link: '/profile' },
+        { icon: 'face', title: 'Sign up', link: '/signup' },
+        { icon: 'lock_open', title: 'Sign in', link: '/signin' },
 
       ]
     }
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Funruns from '@/components/Funrun/Funruns'
+import CreateFunrun from '@/components/Funrun/CreateFunrun'
+import Profile from '@/components/User/Profile'
+import Signup from '@/components/User/Signup'
+import Signin from '@/components/User/Signin'
+
 
 Vue.use(Router)
 
@@ -8,8 +14,36 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/funruns',
+      name: 'Funruns',
+      component: Funruns
+    },
+    {
+      path: '/meetup/new',
+      name: 'CreateFunrun',
+      component: CreateFunrun
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+
+  ],
+
+  mode: 'history'
 })
