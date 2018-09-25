b3196ec51503901d801619152b06f35da6e0f995
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-25 12:10:11 +0100

    trying vue gh pages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <v-app>
+    <v-toolbar>
+      <v-toolbar-title>
+        <v-icon>whatshot</v-icon>
+        Funrun</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>
+          <v-icon left>directions_run</v-icon>
+          View Funruns</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+
+    </main>
   </v-app>
 </template>
 
